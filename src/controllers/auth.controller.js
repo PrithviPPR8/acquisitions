868fbc3e9f5d6a72047f8ed3e4e3ee1c0f8d2903
PrithviPPR8 <prithvirajpatil64@gmail.com>
2025-11-19 14:08:39 +0530

    Implement Auth done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import logger from "#config/logger.js"
+import { createUser } from "#services/auth.service.js";
 import { formatValidationError } from "#utils/format.js";
 import { signUpSchema } from "#validations/auth.validation.js";
+import { jwttoken } from '#utils/jwt.js';
+import { cookies } from '#utils/cookies.js';
 
 export const signup = async (req, res, next) => {
     try {
@@ -13,7 +16,22 @@ export const signup = async (req, res, next) => {
             });
         }
 
-        const { name, email, role } = validationResult.data();
+        const { name, email, password, role } = validationResult.data;
+
+        //AUTH SERVICE
+        const user = await createUser({ name, email, password, role });
+
+        const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+
+        cookies.set(res, 'token', token);
+
+        logger.info(`User registered successfully: ${email}`);
+        res.status(201).json({
+            message: "User registered",
+            user: {
+                id: user.id, name: user.name, email: user.email, role: user.role
+            }
+        })
     } catch (e) {
         logger.error("Sign-up error", e);
 
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
+import logger from "#config/logger.js";
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key-please-change-in-production';
 const JWT_EXPIRES_IN = '1d';
 
-export default jwttoken = {
+export const jwttoken = {
     sign: (payload) => {
         try {
             return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
